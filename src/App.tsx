47ffd515c0a285b66bd488b93fc61a53de4f4e16
Hrishabh Gupta <162270765+hrishabh1103@users.noreply.github.com>
2025-09-18 21:43:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Summarizer from './components/Summarizer';
+// ek baar iska function chk kar lena 
 
 function App() {
   return (
