8526d6ce276bf675758b7ffd8bf3dbecd5bc7fa3
Hrishabh Gupta <162270765+hrishabh1103@users.noreply.github.com>
2025-12-18 23:45:30 +0530

    changed the app style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
-import Summarizer from './components/Summarizer';
+import Summarizer from './components/Summarizer'; 
 // ek baar iska function chk kar lena 
 
 function App() {
